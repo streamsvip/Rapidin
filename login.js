0a835029f439ad4e183b27bf4e6f8a102a5b6cd4
impulso project <velithaz@gmail.com>
2026-06-28 15:02:41 -0500

    Update login.js

--- login.js
+++ login.js
@@ -4,19 +4,27 @@ import {
     signInWithEmailAndPassword
 } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
 
+// FORM
 const form = document.getElementById("loginForm");
 
+// INPUTS
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 
-const showPass = document.getElementById("showLoginPass");
+// BOTÓN OJO
+const toggle = document.getElementById("togglePass");
 
-// 👁️ mostrar contraseña
-showPass.addEventListener("change", () => {
-    password.type = showPass.checked ? "text" : "password";
+// 👁️ mostrar / ocultar contraseña
+toggle.addEventListener("click", () => {
+
+    const isHidden = password.type === "password";
+
+    password.type = isHidden ? "text" : "password";
+
+    toggle.textContent = isHidden ? "🙈" : "👁";
 });
 
-// LOGIN REAL FIREBASE
+// LOGIN FIREBASE
 form.addEventListener("submit", async (e) => {
     e.preventDefault();
 
